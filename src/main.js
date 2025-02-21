dc22746aba585fb579ece215b4b55c8c496aa104
Leonardo Portillo <leonardoandres.portillo@gmail.com>
2025-02-21 18:43:51 -0300

    add variables colors and fonts

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-document.querySelector('#app').innerHTML = `
-  <h1>
-    Hola Mundo
-  </h1>
-`;
+document.addEventListener('DOMContentLoaded', () => {
+	renderHome();
+});
+
+const renderHome = () => {
+	const app = document.getElementById('app');
+
+	app.innerHTML = `
+
+    <section class="quiz">
+      <div class="quiz__title">
+        <h1>Welcome to the <b>Frontend Quiz!</b></h1>
+        <p>Pick a subject to get started.</p>
+      </div>
+
+      <div class="quiz__subject-selector"></div>
+    </section>
+  `;
+};
