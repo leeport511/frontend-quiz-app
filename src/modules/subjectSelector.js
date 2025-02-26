52f72ff30c0f60ae55a7bba6c1b63c7be5236ac2
Leonardo Portillo <leonardoandres.portillo@gmail.com>
2025-02-26 18:46:13 -0300

    refactoring subjectSelector.js

--- src/modules/subjectSelector.js
+++ src/modules/subjectSelector.js
@@ -7,19 +7,24 @@ const subjectSelector = async () => {
 
 	const { quizzes } = await getData();
 
-	quizzes.forEach((quiz) => {
-		console.log(quiz.icon);
+	const fragment = document.createDocumentFragment();
 
+	quizzes.forEach(({ icon, title, iconColor }) => {
 		const subjectImage = document.createElement('img');
-		subjectImage.setAttribute('src', `${quiz.icon}`);
-		subjectImage.setAttribute('alt', `${quiz.title} icon`);
-		subjectImage.classList.add('quiz__subject-selector--img');
-		subjectImage.style.backgroundColor = `${quiz.iconColor}`;
+		subjectImage.className = 'quiz__subject-selector--img';
+		subjectImage.src = icon;
+		subjectImage.alt = `${title} icon`;
+		subjectImage.style.backgroundColor = iconColor;
+
 		const subjectTitle = document.createElement('button');
-		subjectTitle.classList.add('quiz__subject-selector--btn');
-		subjectTitle.textContent = quiz.title;
+		subjectTitle.className = 'quiz__subject-selector--btn';
+		subjectTitle.id = title;
+		subjectTitle.textContent = title;
+
 		subjectTitle.appendChild(subjectImage);
-		subjectsContainer.appendChild(subjectTitle);
+		fragment.appendChild(subjectTitle);
+
+		subjectsContainer.appendChild(fragment);
 	});
 };
 
