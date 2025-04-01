f794c569512ea6f805a67ae8b57d734e3a809c0a
Leonardo Portillo <leonardoandres.portillo@gmail.com>
2025-04-01 19:36:57 -0300

    change getData url

--- src/modules/data/getData.js
+++ src/modules/data/getData.js
@@ -1,5 +1,8 @@
 const getData = async () => {
-	const resp = await fetch('public/data/data.json');
+	const resp = await fetch('/data/data.json');
+	if (!resp.ok)
+		throw new Error(`HTTP error! status: ${resp.status}`);
+
 	const json = await resp.json();
 
 	return json;
