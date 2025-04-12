2095d4c8b360aff4d7b411b984c07856b69ce2f4
Leonardo Portillo <leonardoandres.portillo@gmail.com>
2025-04-12 18:45:18 -0300

    start with dark style transition

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { renderHome } from './modules/quiz/renderHome';
 import { subjectSelector } from './modules/quiz/subjectSelector';
+import { handleDarkBtnAction } from './modules/utils/darkButton';
 
 document.addEventListener('DOMContentLoaded', async () => {
 	renderHome();
 	subjectSelector();
+	handleDarkBtnAction();
 });
---
+++ src/modules/utils/darkButton.js
@@ -0,0 +1,43 @@
+const darkButton = document.getElementById('dark-white-btn');
+const whiteBtnDot = document.querySelector('.btn__white-dot');
+const sunImage = document.getElementById('sun-img');
+const moonImage = document.getElementById('moon-img');
+const body = document.querySelector('body');
+const main = document.querySelector('.main');
+
+export const handleDarkBtnAction = () => {
+	darkButton.addEventListener('click', () => {
+		const quiz = document.querySelector('.quiz');
+		const paragraph = document.querySelector('.quiz__title p');
+		const btnsSelectors = document.querySelectorAll(
+			'.quiz__subject-selector--btn'
+		);
+
+		//* -- dark & light btn
+
+		if (sunImage.src.includes('icon-sun-dark.svg')) {
+			sunImage.src = '/src/assets/images/icon-sun-light.svg'; // versión blanca para dark
+		} else {
+			sunImage.src = '/src/assets/images/icon-sun-dark.svg'; // versión negra para light
+		}
+
+		if (moonImage.src.includes('icon-moon-dark.svg')) {
+			moonImage.src = '/src/assets/images/icon-moon-light.svg'; // versión blanca para dark
+		} else {
+			moonImage.src = '/src/assets/images/icon-moon-dark.svg'; // versión negra para light
+		}
+
+		//* -- globals
+		whiteBtnDot.classList.toggle('translated');
+		body.classList.toggle('dark-theme');
+		main.classList.toggle('main-dark');
+
+		//* -- home
+		quiz.classList.toggle('quiz-dark-theme');
+		paragraph.classList.toggle('dark-paragraph');
+
+		btnsSelectors.forEach((btn) => {
+			btn.classList.toggle('dark-selector');
+		});
+	});
+};
